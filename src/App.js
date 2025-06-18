45d74ae15cd1f76c94b8e4834c79d1a24df314e7
Administrator <gitlab_admin_0a9925@example.com>
2025-06-18 18:10:46 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🌤️ Application Météo</h1>
+      <p>Bienvenue sur mon appli météo !</p>
+    </div>
+  );
+}
+
+export default App;
